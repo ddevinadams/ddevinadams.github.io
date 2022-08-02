b14edd0fe74161251b62e09d1aa8244bdbc6f997
ddevinadams <92732048+ddevinadams@users.noreply.github.com>
2022-08-02 19:13:55 +0700

    Added new files

---
+++ script.py
@@ -0,0 +1,6 @@
+searchBtn= document.getElementById('search-btn')
+mealList= document.getElementById('meal')
+mealDetailsContent= document.select('.meal-details-content')
+mealsCloseBtn= document.getElementById('meals-close-btn')
+
+def getMealList():
